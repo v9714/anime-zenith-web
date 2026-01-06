0ec15904814dd545a2c57c20cce51c87d615170e
kartik <raakartiksinh@gmail.com>
2026-01-06 23:44:40 +0530

    chore: Remove `// NEW` comments from `releaseYear` and `genres` properties.

--- src/services/mangaService.ts
+++ src/services/mangaService.ts
@@ -23,10 +23,10 @@ export interface Manga {
     artist: string;
     rating: string;
     votesCount: number;
-    releaseYear?: number;  // NEW
+    releaseYear?: number;
     isDeleted: boolean;
     updatedAt: string;
-    genres?: MangaGenre[];  // NEW
+    genres?: MangaGenre[];
 }
 
 export interface Chapter {
